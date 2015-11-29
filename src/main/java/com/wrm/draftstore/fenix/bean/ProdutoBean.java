30dc62826b6686b407f22ab5cf0842066ebb6082
Wilson A. de Oliveira <wilsonalves96@live.com>
2015-11-29 20:52:19 -0200

    Buscar produto

--- src/main/java/com/wrm/draftstore/fenix/bean/ProdutoBean.java
+++ src/main/java/com/wrm/draftstore/fenix/bean/ProdutoBean.java
@@ -24,52 +24,52 @@ import javax.faces.context.FacesContext;
 @SessionScoped
 public class ProdutoBean implements Serializable {
 
-  /**
-   * Creates a new instance of ProdutoBean
-   */
-  public ProdutoBean() {
-  }
-  
-  // Produtos Fake
+    /**
+     * Creates a new instance of ProdutoBean
+     */
+    public ProdutoBean() {
+    }
+
+    // Produtos Fake
 //  private List<Produto> ofertasSemana = new ProdutoServiceFakeImpl().listar(0, 10);
-  
-  // Produtos JPA
-  private ProdutoService produtoService = new ProdutoServiceJPAImpl();
-  private List<Produto> ofertasSemana = produtoService.listar(0, 10);
-  
-  private Produto produtoDetalhe;
-  
-  public String carregarDetalhes(){
-    this.produtoDetalhe = this.getProduto();
-    System.out.println("PRODUTO DETALHE: "+this.produtoDetalhe.getModelo());
-    
-    return "detalhe?faces-redirect=true";
-  }
+    // Produtos JPA
+    private ProdutoService produtoService = new ProdutoServiceJPAImpl();
+    private List<Produto> ofertasSemana = produtoService.listar(0, 10);
 
-  public List<Produto> getOfertasSemana() {
-    return ofertasSemana;
-  }
-  
-  public Produto getProduto() {
-    FacesContext contexto = FacesContext.getCurrentInstance();
-    return ofertasSemana.get(getId(contexto)-1);
-  }
-    
-  private int getId(FacesContext fc) {
-    Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
-    return Integer.parseInt(params.get("id"));
-  }
-  
-  public Produto getProdutoDetalhe() {
-    return produtoDetalhe;
-  }
+    private Produto produtoDetalhe;
 
-  public void setProdutoDetalhe(Produto produtoDetalhe) {
-    this.produtoDetalhe = produtoDetalhe;
-  }
-  
-  public List<Produto> getProdutosPorCategoria(Categoria categoria){
-      return this.produtoService.obterPorCategoria(categoria.getIdCategoria(), 0, 10);
-  }
-  
+    public String carregarDetalhes() {
+        this.produtoDetalhe = this.getProduto();
+        System.out.println("PRODUTO DETALHE: " + this.produtoDetalhe.getModelo());
+
+        return "detalhe?faces-redirect=true";
+    }
+
+    public List<Produto> getOfertasSemana() {
+        return ofertasSemana;
+    }
+
+    public Produto getProduto() {
+        FacesContext contexto = FacesContext.getCurrentInstance();
+        Long id = (long) getId(contexto);
+        return produtoService.obter(id);
+    }
+
+    private int getId(FacesContext fc) {
+        Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+        return Integer.parseInt(params.get("id"));
+    }
+
+    public Produto getProdutoDetalhe() {
+        return produtoDetalhe;
+    }
+
+    public void setProdutoDetalhe(Produto produtoDetalhe) {
+        this.produtoDetalhe = produtoDetalhe;
+    }
+
+    public List<Produto> getProdutosPorCategoria(Categoria categoria) {
+        return this.produtoService.obterPorCategoria(categoria.getIdCategoria(), 0, 10);
+    }
+
 }
