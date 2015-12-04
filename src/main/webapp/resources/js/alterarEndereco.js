450c9dbaf0c09ae2c760cbabb43a9bee424680ba
ramonhonorio <ramonaqh@gmail.com>
2015-12-04 11:55:29 -0200

    Cep no alterar endereço funcionando

--- src/main/webapp/resources/js/alterarEndereco.js
+++ src/main/webapp/resources/js/alterarEndereco.js
@@ -1,17 +1,37 @@
-$(function(){
-   $("#formAlterarEndereco\\:cep").change(function(){
-      var cep_code = $(this).val();
-      if( cep_code.length <= 0 ) return;
-      $.get("http://apps.widenet.com.br/busca-cep/api/cep.json", { code: cep_code },
-         function(result){
-            if( result.status!=1 ){
-               alert(result.message || "Houve um erro desconhecido");
-               return;
-            }
-            $("input#formAlterarEndereco\\:estado").val( result.state );
-            $("input#formAlterarEndereco\\:cidade").val( result.city );
-            $("input#formAlterarEndereco\\:bairro").val( result.district );
-            $("input#formAlterarEndereco\\:rua").val( result.address );
-         });
-   });
-});
+//$(function () {
+//  $("#formAlterarEndereco\\:cep").change(function () {
+//    var cep_code = $(this).val();
+//    if (cep_code.length <= 0)
+//      return;
+//    $.get("http://apps.widenet.com.br/busca-cep/api/cep.json", {code: cep_code},
+//    function (result) {
+//      alert('CEP Recebido' + result.address);
+//      if (result.status != 1) {
+//        alert(result.message || "Houve um erro desconhecido");
+//        return;
+//      }
+//      $("input#formAlterarEndereco\\:estado").val(result.state);
+//      $("input#formAlterarEndereco\\:cidade").val(result.city);
+//      $("input#formAlterarEndereco\\:bairro").val(result.district);
+//      $("input#formAlterarEndereco\\:rua").val(result.address);
+//    });
+//  });
+//});
+
+alterarEndereco = function () {
+  var cep_code = $("#formAlterarEndereco\\:cep").val();
+  if (cep_code.length <= 0)
+    return;
+  $.get("http://apps.widenet.com.br/busca-cep/api/cep.json", {code: cep_code},
+  function (result) {
+    
+    if (result.status != 1) {
+      alert(result.message || "Houve um erro desconhecido");
+      return;
+    }
+    $("input#formAlterarEndereco\\:estado").val(result.state);
+    $("input#formAlterarEndereco\\:cidade").val(result.city);
+    $("input#formAlterarEndereco\\:bairro").val(result.district);
+    $("input#formAlterarEndereco\\:rua").val(result.address);
+  });
+};
