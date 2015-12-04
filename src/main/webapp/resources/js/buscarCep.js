83c9356f89d88f82c924652ee9b2427278c16d1d
ramonhonorio <ramonaqh@gmail.com>
2015-12-04 12:00:46 -0200

    Opa, esqueci de tirar um alerta kk

--- src/main/webapp/resources/js/buscarCep.js
+++ src/main/webapp/resources/js/buscarCep.js
@@ -4,7 +4,6 @@ $(function(){
       if( cep_code.length <= 0 ) return;
       $.get("http://apps.widenet.com.br/busca-cep/api/cep.json", { code: cep_code },
          function(result){
-           alert('CEP Recebido'+result.address);
             if( result.status!=1 ){
                alert(result.message || "Houve um erro desconhecido");
                return;
